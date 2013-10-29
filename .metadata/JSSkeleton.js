bff8d33587d4d912956676d59eb2f3f833e8e968
Presley <presleyanne@gmail.com>
2013-10-29 14:00:35 -0400

    Add Javascript to hide Prev button.

--- .metadata/JSSkeleton.js
+++ .metadata/JSSkeleton.js
@@ -1,1 +1,12 @@
 %1$s
+
+// hide Previous buttons because in an experiment you shouldn't
+// be able to double check your previous responses
+function hidePrev(){
+    $('input[value="Prev"]').hide();
+}
+
+
+$(document).ready(function(){
+    hidePrev();
+});

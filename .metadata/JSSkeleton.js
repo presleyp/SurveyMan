cf29d0a1eefa90e1086b86b95bfbd28c9f81a228
Presley <presleyanne@gmail.com>
2013-11-06 11:58:14 -0500

    Update JSSkeleton for use with new JS setup.

--- .metadata/JSSkeleton.js
+++ .metadata/JSSkeleton.js
@@ -1,4 +1,8 @@
 %1$s
+/* This file is for individual customization. Define any functions
+ * you'd like, and call them in customInit, which is called by the
+ * main Javascript file when the document is loaded.
+ */
 
 // hide Previous buttons because in an experiment you shouldn't
 // be able to double check your previous responses
@@ -6,7 +10,7 @@ function hidePrev(){
     $('input[value="Prev"]').hide();
 }
 
-
-$(document).ready(function(){
-    hidePrev();
-});
+// called when a survey is loaded
+function customInit(){
+   hidePrev();
+}
